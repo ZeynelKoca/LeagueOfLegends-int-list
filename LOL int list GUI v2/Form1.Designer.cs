6cc36fdb5a54cbfc9ff55062f094d246e61840a3
Zeynel <zeynel.koca@outlook.com>
2020-01-01 01:45:15 +0100

    Very basic visual GUI startpoint

--- LOL int list GUI v2/Form1.Designer.cs
+++ LOL int list GUI v2/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblClientStatus = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtbxSummonerName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 483);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "League client status:";
+            // 
+            // lblClientStatus
+            // 
+            this.lblClientStatus.AutoSize = true;
+            this.lblClientStatus.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClientStatus.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblClientStatus.Location = new System.Drawing.Point(144, 483);
+            this.lblClientStatus.Name = "lblClientStatus";
+            this.lblClientStatus.Size = new System.Drawing.Size(49, 19);
+            this.lblClientStatus.TabIndex = 0;
+            this.lblClientStatus.Text = "Status";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkGray;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(305, 34);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(55, 27);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtbxSummonerName
+            // 
+            this.txtbxSummonerName.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxSummonerName.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtbxSummonerName.Location = new System.Drawing.Point(46, 34);
+            this.txtbxSummonerName.Name = "txtbxSummonerName";
+            this.txtbxSummonerName.Size = new System.Drawing.Size(253, 27);
+            this.txtbxSummonerName.TabIndex = 2;
+            this.txtbxSummonerName.Text = "Summoner name";
+            this.txtbxSummonerName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtbxSummonerName.Enter += new System.EventHandler(this.txtbxSummonerName_Enter);
+            this.txtbxSummonerName.Leave += new System.EventHandler(this.txtbxSummonerName_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(408, 506);
+            this.Controls.Add(this.txtbxSummonerName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblClientStatus);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblClientStatus;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtbxSummonerName;
     }
 }
 
--- LOL int list GUI v2/Form1.cs
+++ LOL int list GUI v2/Form1.cs
@@ -15,6 +15,41 @@ namespace LOL_int_list_GUI_v2
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void SetColors()
+        {
+            this.BackColor = Color.FromArgb(37, 37, 37);
+            btnAdd.BackColor = Color.FromArgb(80, 80, 80);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbxSummonerName_Enter(object sender, EventArgs e)
+        {
+            if(txtbxSummonerName.Text == "Summoner name")
+            {
+                txtbxSummonerName.Text = "";
+                txtbxSummonerName.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtbxSummonerName_Leave(object sender, EventArgs e)
+        {
+            if (txtbxSummonerName.Text == "")
+            {
+                txtbxSummonerName.Text = "Summoner name";
+                txtbxSummonerName.ForeColor = Color.Silver;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
